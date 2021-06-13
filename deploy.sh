e0be3eb2a6184e2970e19ea9ca347288e547e7ed
drsherlock <zain.delhi27@hotmail.com>
2021-06-13 18:19:36 +0530

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-docker ps --filter "name=image-generator-web"
-docker pull $IMAGE
-docker rm -f image-generator-web || true
-docker run -d \
-           --name image-generator-web \
-           --restart unless-stopped \
-           -p 80:3000 \
-           $IMAGE
-docker ps --filter "name=image-generator-web"
