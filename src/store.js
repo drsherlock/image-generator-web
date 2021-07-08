00d1a4386a87a98644b5394f0ca9bb5aa0fb6712
drsherlock <zain.delhi27@hotmail.com>
2021-07-09 00:27:09 +0530

    Use redux devtools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export function configureStore() {
-  const store = createStore(reducers, applyMiddleware(thunk));
+  const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
   return store;
 }
 
