d52e634288a3ddecb413af7cbc255acbef20997f
drsherlock <zain.delhi27@hotmail.com>
2021-07-10 23:09:58 +0530

    Fix API URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const config = {
   API:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8080"
-      : "http://image-generator-api.centralindia.cloudapp.azure.com/"
+      : "http://image-generator-api.centralindia.cloudapp.azure.com"
 };
